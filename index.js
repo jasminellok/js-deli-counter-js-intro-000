db62e1e38118b57b77b2d953d36c396dac7c804a
Jasmine Lok <jasminellok@gmail.com>
2020-04-11 00:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line) {
   if (line.length===0) {
     return "The line is currently empty."
   } else {
-    for (i=0; i<=line.length; i+=1) {
+    for (let i=1; i<=line.length; i+=1) {
       listCustomer += `${i}. ${line[i]}`
     }
   }
